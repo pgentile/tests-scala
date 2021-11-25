1fa04cf79825441612edecc7a1b407b2e7c4a38f
Pierre Gentile <pierre.gentile.perso@gmail.com>
2021-11-25 22:33:32 +0100

    Traverse

--- src/main/scala/fp/traverse/TraverseApp.scala
+++ src/main/scala/fp/traverse/TraverseApp.scala
@@ -4,10 +4,15 @@ package fp.traverse
 import cats.syntax.all._
 
 import scala.util.Try
+import scala.math.Numeric._
 
 object TraverseApp extends App {
 
   private def parseInt(item: String): Try[Int] = Try(item.toInt)
+
+  private def onlyEvenNumbers[T](number: T)(implicit numeric: Integral[T]): Option[T] = {
+    if (numeric.rem(number, numeric.fromInt(2)) == numeric.zero) Some(number) else None
+  }
 
   {
     val numbers = Seq("1", "2", "3")
@@ -24,6 +29,12 @@ object TraverseApp extends App {
   {
     val numbers = Seq("1", "2", "3").as(Seq(1, 2))
     println(numbers)
+  }
+
+  {
+    val numbers: Seq[Int] = (1 to 10)
+    val result: Option[Seq[Int]] = numbers.traverse(onlyEvenNumbers(_))
+    println(result)
   }
 
 }
