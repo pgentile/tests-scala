1b49437badf341f4d984ea3802c1944ffa0ecccb
Pierre Gentile <pierre.gentile.perso@gmail.com>
2021-12-01 20:47:33 +0100

    Upgraded versions

--- build.sbt
+++ build.sbt
@@ -4,19 +4,18 @@ version := "0.1"
 
 scalaVersion := "2.13.6"
 
-val catsVersion = "2.3.0"
 val slf4jVersion = "1.7.32"
 val akkaVersion = "2.6.17"
 
-libraryDependencies += "org.scala-lang.modules" %% "scala-parallel-collections" % "1.0.3"
-libraryDependencies += "org.typelevel" %% "cats-core" % catsVersion
-libraryDependencies += "org.typelevel" %% "cats-effect" % catsVersion
+libraryDependencies += "org.scala-lang.modules" %% "scala-parallel-collections" % "1.0.4"
+libraryDependencies += "org.typelevel" %% "cats-core" % "2.6.1"
+libraryDependencies += "org.typelevel" %% "cats-effect" % "3.2.8"
 libraryDependencies += "org.apache.kafka" % "kafka-clients" % "2.8.0"
 libraryDependencies += "com.beachape" %% "enumeratum" % "1.7.0"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.9" % Test
 libraryDependencies += "org.scalatestplus" %% "scalacheck-1-15" % "3.2.9.0" % Test
-libraryDependencies += "io.github.embeddedkafka" %% "embedded-kafka" % "2.8.0" % Test
+libraryDependencies += "io.github.embeddedkafka" %% "embedded-kafka" % "3.0.0" % Test
 
 libraryDependencies += "org.slf4j" % "slf4j-api" % slf4jVersion
 libraryDependencies += "org.slf4j" % "slf4j-simple" % slf4jVersion
--- src/main/scala/catsio/CatsIOApp.scala
+++ src/main/scala/catsio/CatsIOApp.scala
@@ -1,25 +1,22 @@
 package org.example.testsscala
 package catsio
 
-import cats.effect.{ContextShift, IO}
+import cats.effect.{IO, IOApp}
 
-import scala.concurrent.ExecutionContext
 import scala.util.Random
 
-object CatsIOApp extends App {
+object CatsIOApp extends IOApp.Simple {
 
-  implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
-
-  val hello = IO {
+  private val hello = IO {
     val threadName = Thread.currentThread.getName
     val visitorId = Random.nextInt(1000)
     println(s"[$threadName] Hello, visitor #$visitorId")
     visitorId
   }
 
-  val aboutVisitor = (visitorId: Int) => IO { println(s"This is visitor #$visitorId") }
+  private val aboutVisitor = (visitorId: Int) => IO { println(s"This is visitor #$visitorId") }
 
-  val error = IO.raiseError(new IllegalStateException("Fuck"))
+  private val error = IO.raiseError(new IllegalStateException("Fuck"))
 
   val program: IO[Int] = for {
     visitor1 <- hello
@@ -35,11 +32,6 @@ object CatsIOApp extends App {
     }
   } yield x
 
-  val result: Int = program
-    // .handleErrorWith { error =>
-    //   IO(println(s"Got some error: $error")) *> IO.pure(-1)
-    // }
-    .unsafeRunSync()
-  println(result)
+  override def run: IO[Unit] = program.as(())
 
 }
