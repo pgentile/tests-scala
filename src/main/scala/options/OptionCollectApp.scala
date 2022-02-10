9bbd42fdc2a4d256dd2d140c6313439e55341aa7
Pierre Gentile <pierre.gentile@iadvize.com>
2022-02-10 19:37:54 +0100

    Collect on Options

--- src/main/scala/options/OptionCollectApp.scala
+++ src/main/scala/options/OptionCollectApp.scala
@@ -4,11 +4,11 @@ package options
 object OptionCollectApp {
 
   def main(args: Array[String]): Unit = {
-    Seq("Jean Bon", "Guy Gnol").foreach { name =>
-      val result = Some(name).collect {
+    Seq("Jean Bon", "Guy Gnol").map(Some(_)).foreach { maybeName =>
+      val result = maybeName.collect {
         case s"Jean ${lastName}" => lastName
       }
-      println(s"Last name for $name: $result")
+      println(s"Last name for $maybeName: $result")
     }
   }
 
