695318a5f15d6618b7544648e0da376a38399d8b
Pierre Gentile <pierre.gentile@iadvize.com>
2022-05-30 19:45:20 +0200

    Seq pattern matching

--- src/main/scala/patternmatching/SeqPatternMatchingMain.scala
+++ src/main/scala/patternmatching/SeqPatternMatchingMain.scala
@@ -6,20 +6,29 @@ import scala.collection.immutable.LinearSeq
 object SeqPatternMatchingMain {
 
   def main(args: Array[String]): Unit = {
-    val emptyList = Seq.empty[Int]
-    val one = 4 :: Nil
-    val many = LinearSeq(1, 2, 3, 5, 8, 13, 21)
-    val two = many.slice(0, 2)
+    {
+      val emptyList = Seq.empty[Int]
+      val one = 4 :: Nil
+      val many = LinearSeq(1, 2, 3, 5, 8, 13, 21)
+      val two = many.slice(0, 2)
 
-    val target: Seq[Int] = two
+      val target: Seq[Int] = two
 
-    val result = target match {
-      case Nil => "Empty"
-      case first +: second +: remaining => s"$first, $second and ${remaining.length} remaining items"
-      case _ => "Unknown"
+      val result = target match {
+        case Nil => "Empty"
+        case first +: second +: remaining => s"$first, $second and ${remaining.length} remaining items"
+        case _ => "Unknown"
+      }
+
+      println(result)
     }
-
-    println(result)
+    {
+      val target = Seq(1, 2, 3)
+      val result = target match {
+        case _beginning :+ last => s"Last is $last"
+      }
+      println(result)
+    }
   }
 
 }
